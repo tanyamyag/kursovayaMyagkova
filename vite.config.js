ca30d5879d20882188352563a0c41389315fdc64
unknown <Tatyana>
2025-12-14 16:57:10 +1000

    Настроен Vite: минификация и защита исходного кода

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,9 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     port: 5173
+  },
+  build: {
+    sourcemap: false, // отключает генерацию source maps
+    minify: 'terser'  // включает минификацию для защиты исходного кода
   }
 })
